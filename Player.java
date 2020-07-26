d4134843999ded70c0185ed6e147bcb03f0ac0fe
Pkhokgawe <50570710+Pkhokgawe@users.noreply.github.com>
2020-07-27 11:45:57 +1200

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,7 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.30.0.5059.b674b8115 modeling language!*/
+
+
 import java.util.*;
 
 // line 8 "model.ump"
@@ -72,13 +76,42 @@ public class Player
   }
 
   // line 14 "model.ump"
-   public void suggest(CardChar character , CardRoom room , CardWeapon weapon){
-    
+   public Card suggest(CardChar character , CardRoom room , CardWeapon weapon,Player other){
+      return other.refute(character,room,weapon);
   }
 
   // line 17 "model.ump"
-   public boolean refute(Card card){
-    return cards.contains(card);
+   public Card refute(CardChar character , CardRoom room , CardWeapon weapon){
+    List<Card> options = new ArrayList<>();
+    if(cards.contains(character)){
+      options.add(character);
+    }
+     if(cards.contains(room)){
+       options.add(room);
+     }
+     if(cards.contains(weapon)){
+       options.add(weapon);
+     }
+     int size = options.size();
+     if(size == 1){
+       return options.get(0);
+     } else if(size == 0){ 
+       return null;
+     } else {
+       Scanner scan = new Scanner(System.in);
+       System.out.println("These are your options: " + options.toString() + "\n what would you like to refute ? Please select an index [ 1 , 2 , 3] : ");
+       
+       //keep asking the user until there's a valid index
+       while(true) {
+         try {
+           Card card = options.get(scan.nextInt()-1);
+           scan.close();
+           return card;
+         } catch (Exception e) {
+           System.out.println("Invalid input please enter a valid index\n");
+         }
+       }
+     }
     
   }
 
